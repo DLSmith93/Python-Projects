586b04127d50517c7d3ff015a30bbdf3fc1cfe94
DLSmith93 <baumbor@hotmail.com>
2023-12-02 15:05:31 -0800

    Started Python Practice

---
+++ Challanges/challange1.py
@@ -0,0 +1,129 @@
+#################################################################
+## This python file is for various coding challanges in python ##
+#################################################################
+
+#print ("Hello World")
+
+#x = 5
+#y = 7
+
+#if(y > x):
+    #print("Y is larger than X!")
+
+#print(x + y)
+
+#myString = "This is a string"
+#fname = "Devin"
+#lname = "Smith"
+
+#print(myString)
+
+#print(len(myString))
+
+#print(myString[0])
+
+#print("My name is {} {}, this is python!".format(fname, lname))
+
+#print("My name is " + fname + " "  + lname)
+
+#newName = fname.upper()
+
+#print(newName)
+
+#x **= 2
+
+#print(x)
+
+#if(x > 20 & y < 10):
+    #print("This is [rinting because an and logical has been met!")
+
+#print("Devin" in fname)
+
+###############################
+## This is pracricing tuples ##
+###############################
+
+#animal = ( "zebra", "alligator", "lion", "ox", "wolf")
+
+#listofAnimals = list(animal)
+#print(listofAnimals)
+
+#testString = "Hello! I am pleased to meat you"
+#newString = list(testString)
+#print(newString)
+
+#newString = testString.split()
+#print(newString)
+
+#print("This is printing out the tuple")
+#for i in range(len(animal)):
+    #print(animal[i])
+
+#print("this is how many items are in the tuple")
+#count = animal.count("wolf")
+#print(count)
+
+##############################
+## This is pracricing lists ##
+##############################
+
+#myList = [2,3,4,5]
+#list1 = [7,8,9,10]
+
+#print("My list of ints will print.")
+#for i in myList:
+    #print(i)
+
+#myList.append(6)
+
+#print("This is my list with a 6 added to the end.")
+#for i in myList:
+    #print(i)
+
+#myList.reverse()
+
+#print("This is my list in reverse.")
+#for i in myList:
+    #print(i)
+
+#newList = myList.copy()
+
+#print("My is a copy of my list.")
+#for i in newList:
+    #print(i)
+
+#for i in list1:
+    #myList.append(i)
+
+#print('This is two lists concatenated')
+#for i in myList:
+    #print(i)
+
+
+#############################
+## This is pracricing sets ##
+#############################
+
+#myset = {"wedge", "putter", "driver"}
+
+#print(myset)
+
+#myset.add("iron")
+
+#print(myset)
+
+#myset.remove("putter")
+
+#print(myset)
+
+#numSet1 = {1,2,3,4,5}
+
+#numSet2 = {1,3,5,7,9}
+
+#print(numSet1.difference(numSet2))
+#print(numSet2.difference(numSet1))
+
+######################################
+## This is pracricing disctionaries ##
+######################################
+
